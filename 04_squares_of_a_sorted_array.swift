e1a9ec0e079f025463d7ae5ee89806131df5d71a
Andy Wong <wongandy97@gmail.com>
2021-09-26 13:59:27 -0400

    04 1/2 completed b/c there is as better method to this

---
+++ 04_squares_of_a_sorted_array.swift
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+// This solution works but there is a bianry method that works better 
+// class Solution {
+//     func sortedSquares(_ nums: [Int]) -> [Int] {
+//         var sortedSquaredArray = [Int]()
+        
+//         for num in nums {
+//             sortedSquaredArray.append(num * num)
+//         }
+        
+//         return sortedSquaredArray.sorted(by: <)
+//     }
+// }
